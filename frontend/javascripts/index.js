f7161787c3d589409369a40e9c29f7dff074f91e
Alexina Chong <alexina.chong@gmail.com>
2018-02-15 13:47:21 -0800

    Style chart

--- frontend/javascripts/index.js
+++ frontend/javascripts/index.js
@@ -1,3 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
+  let decade = '1940s';
 
+  let decadeTag = document.getElementById("decade-name");
+  decadeTag.innerHTML = "Most common lyrics in the " + decade;
+
+  const changeDecade = (yr) => {
+    decade = yr;
+    decadeTag.innerHTML = "Most common lyrics in the " + decade;
+
+  };
 });
+
+// let decade = '1940s';
+//
+// let decadeTag = document.getElementById("decade-name");
+// console.log(decadeTag);
+// decadeTag.innerHTML = "Most common lyrics in the " + decade;
+// console.log(decadeTag.innerHTML);
+//
+// const changeDecade = (yr) => {
+//   decade = yr;
+//   decadeTag.innerHTML = "Most common lyrics in the " + decade;
+//   return decadeTag;
+// };
+
+const openDecade = (evt, yr) => {
+  let i, x, tablinks;
+  x = document.getElementsByClassName("year");
+  for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" decade-selector-selected", "");
+  }
+  document.getElementById(yr).style.display = "block";
+  evt.currentTarget.className += " decade-selector-selected";
+  decade = yr;
+  decadeTag.innerHTML = "Most common lyrics in the " + decade;
+};
