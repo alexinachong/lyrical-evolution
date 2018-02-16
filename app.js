dbef7f9b0e8f3f30766060d8daff0f40c8932b07
Alexina Chong <alexina.chong@gmail.com>
2018-02-15 22:56:41 -0800

    Add secure jQuery link

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT;
 
 var knex = require('knex')({
   client: 'pg',
-  connection: process.env.PG_CONNECTION_STRING
+  connection: process.env.DATABASE_URL + '?ssl=true'
 });
 
 app.get('/', (req, res) => {
