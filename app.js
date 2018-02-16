83853b72cc02ec582368edf642118254ae3f0b78
Alexina Chong <alexina.chong@gmail.com>
2018-02-16 01:36:59 -0800

    Add limit to database query

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.static('frontend'));
 
 app.get('/decade/:decade', (req, res) => {
   let decade = req.params.decade;
-  knex('decades').where({decade: decade}).first('word_counts').then((wc) => {
+  knex('decades').where({decade: decade}).first('word_counts').limit(1000).then((wc) => {
     let array = JSON.parse(wc.word_counts);
     res.json({data: array});
   });
